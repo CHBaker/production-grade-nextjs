d57595eef88cbb4d67840908e45c2822c8777c1b
CHBaker <charlesprobaker@gmail.com>
2022-10-18 10:41:25 -0600

    FEAT: API calls/DB updates for CRUD
    - CRUD folder && file

---
+++ pages/api/folder/index.ts
@@ -0,0 +1,22 @@
+import nc from 'next-connect'
+import { folder } from '../../../db'
+import middleware from '../../../middleware/all'
+import onError from '../../../middleware/error'
+import { Request } from '../../../types'
+
+const handler = nc({
+    onError,
+})
+
+handler.use(middleware)
+
+handler.post(async (req: Request, res) => {
+    const newFolder = await folder.createFolder(req.db, {
+        ...req.body,
+        createdBy: req.user.id,
+    })
+
+    res.send({ data: newFolder })
+})
+
+export default handler
